4a5b2cae7704f17e4edbfe0fa5bef57d480e6686
aaa <aaa@aaa.com>
2019-03-28 10:58:24 +0200

    190328 update

--- snapshots_school_scripts/john_week_plan.sh
+++ snapshots_school_scripts/john_week_plan.sh
@@ -2,9 +2,39 @@
 # WEEKNUMBER=17 #TEST
 
 
+
+
+#20181214
+if [ "$WEEKNUMBER" == '49' ] || [ "$WEEKNUMBER" == '49' ]
+then
+#--------------------- this is for Full week-all classes events    
+leafpad "WEEK Christmas-xmas ptA kidspedia ramkid  wl14  activated --- Week number: $WEEKNUMBER" &
+ironstartincognito ""$SWFlocal"ramkidpedia_vol04_xmas1.html "$SWFlocal"ramkid_giortes_xmas_pt1.html"
+ironstartincognito ""$SWFlocal"ramkidpedia_vol04_xmas1.html?probeserver "$SWFlocal"ramkid_giortes_xmas_pt1.html"
+ironstartincognito ""$SWFlocal"ramkidpedia_vol04_xmas1.html "$SWFlocal"ramkid_giortes_xmas_pt1.html"
+ironstartincognito ""$SWFlocal"ramkidpedia_vol04_xmas1.html "$SWFlocal"ramkid_giortes_xmas_pt1.html"
+ironstartincognito ""$SWFlocal"ramkidpedia_vol04_xmas1.html "$SWFlocal"ramkid_giortes_xmas_pt1.html"
+
+fi
+
+#20181214
+if [ "$WEEKNUMBER" == '50' ] || [ "$WEEKNUMBER" == '51' ]
+then
+#--------------------- this is for Full week-all classes events    
+leafpad "WEEK Christmas-xmas ptB wl15-wl16   activated --- Week number: $WEEKNUMBER" &
+ironstartincognito ""$SWFgiortes"index_xmas.html"
+ironstartincognito ""$SWFgiortes"index_xmas.html?probeserver"
+ironstartincognito ""$SWFgiortes"index_xmas.html"
+ironstartincognito ""$SWFgiortes"index_xmas.html"
+ironstartincognito ""$SWFgiortes"index_xmas.html"
+
+fi
+
+
+#201902118-22
 if [ "$WEEKNUMBER" == '07' ] || [ "$WEEKNUMBER" == '07' ]
 then
-#--------------------- this is for Full week-all classes events    201902118-22
+#--------------------- this is for Full week-all classes events    
 leafpad "WEEK halloween ptA (ramkid_invitation_party , swf_ramkid_cds2k5/Ramkid_12feb_apokries , KidsPedia/kids_ePedia32cd/Volume13 )   activated --- Week number: $WEEKNUMBER" &
 ironstartincognito ""$SWFgiortes"other/ramkid_invitation_party/index.html "$SWFpath"ramkid_giortes_apokries_pt1.html"
 ironstartincognito ""$SWFgiortes"other/ramkid_invitation_party/index.html "$SWFpath"ramkid_giortes_apokries_pt1.html"
@@ -14,6 +44,7 @@ ironstartincognito ""$SWFgiortes"other/ramkid_invitation_party/index.html "$SWFp
 ironstartincognito ""$SWFgiortes"other/ramkid_invitation_party/index.html "$SWFpath"ramkid_giortes_apokries_pt1.html"
 fi
 
+#20190225-08
 if [ "$WEEKNUMBER" == '08' ] || [ "$WEEKNUMBER" == '09' ]
 then
 #--------------------- this is for Full week-all classes events 
@@ -26,7 +57,7 @@ ironstartincognito ""$SWFgiortes"index_halloween.html"
 ironstartincognito ""$SWFgiortes"index_halloween.html"
 fi
 
-
+#20190312-18
 if [ "$WEEKNUMBER" == '11' ] || [ "$WEEKNUMBER" == '11' ]
 then
 #--------------------- this is for Full week-all classes events 
@@ -40,7 +71,7 @@ ironstartincognito ""$SWFlocal"pack_url_param.html?url1=coding/Rover_nasa_GREEK1
 fi
 
 
-
+#20190319-30 
 if [ "$WEEKNUMBER" == '12' ] || [ "$WEEKNUMBER" == '13' ]
 then
 #--------------------- this is for Full week-all classes events 
@@ -52,6 +83,7 @@ ironstartincognito ""$SWFlocal"pack_url_param.html?url1=graphics_/paint_artpad_y
 ironstartincognito ""$SWFlocal"pack_url_param.html?url1=graphics_/paint_artpad_y8_GREEK_noNavAdUrl.swf\&url2=graphics_/paint_lily_paint_magic_GOOD_PALLETE_NoNavAdUrl_!!.swf\&url3=graphics_/animate_draw_your_cartoon_character_GREEK1_noAdsUrl.swf\&url4=pack_A02.html\&url5=GFX1\&url6=sumopaint1025_GREEK_NoNavUrl1_v4b_progray.swf&probeserver"
 fi
 
+#20190326
 if [ "$WEEKNUMBER" == '14' ] || [ "$WEEKNUMBER" == '14' ]
 then
 #--------------------- this is for Full week-all classes events 
@@ -90,11 +122,11 @@ if [ "$WEEKNUMBER" == '16' ] || [ "$WEEKNUMBER" == '16' ]
 then
 #--------------------- this is for Full week-all classes events 
 leafpad "WEEK easter ptA    activated --- Week number: $WEEKNUMBER" &
-ironstartincognito ""$SWFlocal"ramkidpedia_vol20_pasxa1.html"
-ironstartincognito ""$SWFlocal"ramkidpedia_vol20_pasxa1.html"
-ironstartincognito ""$SWFlocal"ramkidpedia_vol20_pasxa1.html"
-ironstartincognito ""$SWFlocal"ramkidpedia_vol20_pasxa1.html"
-ironstartincognito ""$SWFlocal"ramkidpedia_vol20_pasxa1.html"
+ironstartincognito ""$SWFlocal"ramkidpedia_vol20_pasxa1.html "$SWFlocal"ramkid_giortes_pasxa_pt1.html"
+ironstartincognito ""$SWFlocal"ramkidpedia_vol20_pasxa1.html "$SWFlocal"ramkid_giortes_pasxa_pt1.html"
+ironstartincognito ""$SWFlocal"ramkidpedia_vol20_pasxa1.html "$SWFlocal"ramkid_giortes_pasxa_pt1.html"
+ironstartincognito ""$SWFlocal"ramkidpedia_vol20_pasxa1.html "$SWFlocal"ramkid_giortes_pasxa_pt1.html"
+ironstartincognito ""$SWFlocal"ramkidpedia_vol20_pasxa1.html "$SWFlocal"ramkid_giortes_pasxa_pt1.html"
 fi
 
 if [ "$WEEKNUMBER" == '17' ] || [ "$WEEKNUMBER" == '17' ]
