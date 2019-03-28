2e9745135e267f1d88b6ce5691903aabfe671545
aaa <aaa@aaa.com>
2019-03-28 09:22:59 +0200

    190328 added week planner script (for full week events)

---
+++ snapshots_school_scripts/john_week_plan.sh
@@ -0,0 +1,112 @@
+# date +%V
+# WEEKNUMBER=17 #TEST
+
+
+if [ "$WEEKNUMBER" == '07' ] || [ "$WEEKNUMBER" == '07' ]
+then
+#--------------------- this is for Full week-all classes events    201902118-22
+leafpad "WEEK halloween ptA (ramkid_invitation_party , swf_ramkid_cds2k5/Ramkid_12feb_apokries , KidsPedia/kids_ePedia32cd/Volume13 )   activated --- Week number: $WEEKNUMBER" &
+ironstartincognito ""$SWFgiortes"other/ramkid_invitation_party/index.html "$SWFpath"ramkid_giortes_apokries_pt1.html"
+ironstartincognito ""$SWFgiortes"other/ramkid_invitation_party/index.html "$SWFpath"ramkid_giortes_apokries_pt1.html"
+ironstartincognito ""$SWFgiortes"other/ramkid_invitation_party/index.html "$SWFpath"ramkid_giortes_apokries_pt1.html"
+ironstartincognito ""$SWFgiortes"other/ramkid_invitation_party/index.html "$SWFpath"ramkid_giortes_apokries_pt1.html"
+ironstartincognito ""$SWFgiortes"other/ramkid_invitation_party/index.html "$SWFpath"ramkid_giortes_apokries_pt1.html"
+ironstartincognito ""$SWFgiortes"other/ramkid_invitation_party/index.html "$SWFpath"ramkid_giortes_apokries_pt1.html"
+fi
+
+if [ "$WEEKNUMBER" == '08' ] || [ "$WEEKNUMBER" == '09' ]
+then
+#--------------------- this is for Full week-all classes events 
+leafpad "WEEK halloween ptB (for 2 weeks  games)   activated --- Week number: $WEEKNUMBER    20190225-08" &
+ironstartincognito ""$SWFgiortes"index_halloween.html"
+ironstartincognito ""$SWFgiortes"index_halloween.html"
+ironstartincognito ""$SWFgiortes"index_halloween.html"
+ironstartincognito ""$SWFgiortes"index_halloween.html"
+ironstartincognito ""$SWFgiortes"index_halloween.html"
+ironstartincognito ""$SWFgiortes"index_halloween.html"
+fi
+
+
+if [ "$WEEKNUMBER" == '11' ] || [ "$WEEKNUMBER" == '11' ]
+then
+#--------------------- this is for Full week-all classes events 
+leafpad "coding Rover_nasa , tortuga(local) activated --- Week number: $WEEKNUMBER" &
+ironstartincognito ""$SWFlocal"pack_url_param.html?url1=coding/Rover_nasa_GREEK1.swf\&url2=coding/Tiny-explorers__nice_puzzle_education_unencr_noAds_ok.swf\&url3="$GAMESEDU"tortuga-kids-logo-gr\&url4=pack_A02.html\&url5=ΚΩΔΙΚΑΣ&probeserver"
+ironstartincognito ""$SWFlocal"pack_url_param.html?url1=coding/Rover_nasa_GREEK1.swf\&url2=coding/Tiny-explorers__nice_puzzle_education_unencr_noAds_ok.swf\&url3="$GAMESEDU"tortuga-kids-logo-gr\&url4=pack_A02.html\&url5=ΚΩΔΙΚΑΣ&probeserver"
+ironstartincognito ""$SWFlocal"pack_url_param.html?url1=coding/Rover_nasa_GREEK1.swf\&url2=coding/Tiny-explorers__nice_puzzle_education_unencr_noAds_ok.swf\&url3="$GAMESEDU"tortuga-kids-logo-gr\&url4=pack_A02.html\&url5=ΚΩΔΙΚΑΣ&probeserver"
+ironstartincognito ""$SWFlocal"pack_url_param.html?url1=coding/Rover_nasa_GREEK1.swf\&url2=coding/Tiny-explorers__nice_puzzle_education_unencr_noAds_ok.swf\&url3="$GAMESEDU"tortuga-kids-logo-gr\&url4=pack_A02.html\&url5=ΚΩΔΙΚΑΣ&probeserver"
+ironstartincognito ""$SWFlocal"pack_url_param.html?url1=coding/Rover_nasa_GREEK1.swf\&url2=coding/Tiny-explorers__nice_puzzle_education_unencr_noAds_ok.swf\&url3="$GAMESEDU"tortuga-kids-logo-gr\&url4=pack_A02.html\&url5=ΚΩΔΙΚΑΣ&probeserver"
+
+fi
+
+
+
+if [ "$WEEKNUMBER" == '12' ] || [ "$WEEKNUMBER" == '13' ]
+then
+#--------------------- this is for Full week-all classes events 
+leafpad "WEEK 12,13 GFX1 ptA, PTB   activated --- Week number: $WEEKNUMBER" &
+ironstartincognito ""$SWFlocal"pack_gfx1.html?probeserver"
+ironstartincognito ""$SWFlocal"pack_gfx1.html?probeserver"
+ironstartincognito ""$SWFlocal"pack_gfx1.html?probeserver"
+ironstartincognito ""$SWFlocal"pack_url_param.html?url1=graphics_/paint_artpad_y8_GREEK_noNavAdUrl.swf\&url2=graphics_/paint_lily_paint_magic_GOOD_PALLETE_NoNavAdUrl_!!.swf\&url3=graphics_/animate_draw_your_cartoon_character_GREEK1_noAdsUrl.swf\&url4=pack_A02.html\&url5=GFX1\&url6=sumopaint1025_GREEK_NoNavUrl1_v4b_progray.swf&probeserver"
+ironstartincognito ""$SWFlocal"pack_url_param.html?url1=graphics_/paint_artpad_y8_GREEK_noNavAdUrl.swf\&url2=graphics_/paint_lily_paint_magic_GOOD_PALLETE_NoNavAdUrl_!!.swf\&url3=graphics_/animate_draw_your_cartoon_character_GREEK1_noAdsUrl.swf\&url4=pack_A02.html\&url5=GFX1\&url6=sumopaint1025_GREEK_NoNavUrl1_v4b_progray.swf&probeserver"
+fi
+
+if [ "$WEEKNUMBER" == '14' ] || [ "$WEEKNUMBER" == '14' ]
+then
+#--------------------- this is for Full week-all classes events 
+leafpad "WEEK GFX2 ptA, PTB    activated --- Week number: $WEEKNUMBER" &
+
+cat >> /tmp/ΟΔΗΓΙΕΣ.txt << EOF
+Week number: $WEEKNUMBER
+ΑΣΚΗΣΗ 1:
+Α) ΒΑΛΤΕ ΤΟΝ GARFIELD
+Β) ΒΑΛΤΕ ΕΝΑ ΣΚΥΛΟ
+Γ) ΒΑΛΤΕ ΣΥΝΕΦΑΚΙ ΣΤΟΝ GARFIELD
+Δ) ΓΡΑΨΤΕ (ΣΤΑ ΕΛΛΗΝΙΚΑ): "ΓΑΒ"
+Ε) 2η ΕΙΚΌΝΑ
+EOF
+leafpad /tmp/ΟΔΗΓΙΕΣ.txt &
+
+ironstartincognito ""$SWFlocal"pack_gfx2.html?timer3&probeserver"
+ironstartincognito ""$SWFlocal"pack_gfx2.html?timer3&probeserver"
+ironstartincognito ""$SWFlocal"pack_gfx2.html?timer3&probeserver"
+ironstartincognito ""$SWFlocal"pack_gfx2.html?timer3&probeserver"
+ironstartincognito ""$SWFlocal"pack_gfx2.html?timer3&probeserver"
+fi
+
+if [ "$WEEKNUMBER" == '15' ] || [ "$WEEKNUMBER" == '15' ]
+then
+#--------------------- this is for Full week-all classes events 
+leafpad "WEEK TANK ,pixbot    activated --- Week number: $WEEKNUMBER" &
+ironstartincognito ""$GAMESEDU"Code-Commander-gr/index_dot_links.html "$GAMESEDU"blockly-games/el/index.html?lang=el "$SWFlocal"pack_A02.html http://pixbot.patatakia.tk "
+ironstartincognito ""$GAMESEDU"Code-Commander-gr/index_dot_links.html "$GAMESEDU"blockly-games/el/index.html?lang=el "$SWFlocal"pack_A02.html http://pixbot.patatakia.tk "
+ironstartincognito ""$GAMESEDU"Code-Commander-gr/index_dot_links.html "$GAMESEDU"blockly-games/el/index.html?lang=el "$SWFlocal"pack_A02.html http://pixbot.patatakia.tk "
+ironstartincognito ""$GAMESEDU"Code-Commander-gr/index_dot_links.html "$GAMESEDU"blockly-games/el/index.html?lang=el "$SWFlocal"pack_A02.html http://pixbot.patatakia.tk "
+ironstartincognito ""$GAMESEDU"Code-Commander-gr/index_dot_links.html "$SWFlocal"pack_A02.html http://pixbot.patatakia.tk"
+fi
+
+if [ "$WEEKNUMBER" == '16' ] || [ "$WEEKNUMBER" == '16' ]
+then
+#--------------------- this is for Full week-all classes events 
+leafpad "WEEK easter ptA    activated --- Week number: $WEEKNUMBER" &
+ironstartincognito ""$SWFlocal"ramkidpedia_vol20_pasxa1.html"
+ironstartincognito ""$SWFlocal"ramkidpedia_vol20_pasxa1.html"
+ironstartincognito ""$SWFlocal"ramkidpedia_vol20_pasxa1.html"
+ironstartincognito ""$SWFlocal"ramkidpedia_vol20_pasxa1.html"
+ironstartincognito ""$SWFlocal"ramkidpedia_vol20_pasxa1.html"
+fi
+
+if [ "$WEEKNUMBER" == '17' ] || [ "$WEEKNUMBER" == '17' ]
+then
+#--------------------- this is for Full week-all classes events 
+leafpad "WEEK easter ptB    activated --- Week number: $WEEKNUMBER" &
+ironstartincognito ""$SWFgiortes"index_easter.html"
+ironstartincognito ""$SWFgiortes"index_easter.html"
+ironstartincognito ""$SWFgiortes"index_easter.html"
+ironstartincognito ""$SWFgiortes"index_easter.html"
+ironstartincognito ""$SWFgiortes"index_easter.html"
+ironstartincognito ""$SWFgiortes"index_easter.html"
+fi
+
+################## END OF FULL WEEK EVENTS #################################
