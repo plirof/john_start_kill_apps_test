1ea9049e68f13063fa91c233fc5e36047e32092b
aaa <aaa@aaa.com>
2022-05-27 11:08:46 +0300

    220520 - added delay to download tries

--- wipeout-update-script/john_downloads_remote_dim-000.sh
+++ wipeout-update-script/john_downloads_remote_dim-000.sh
@@ -15,7 +15,11 @@ cd /tmp ;
 FILE="john_downloads_remote_dim-000.sh";
 URL="http://swf-dim.matheme.win/4class_sfs/";
 #change FILE to sch dim name
-wget "$URL""$FILE" -T 10  --directory-prefix=/tmp/;
+#wget "$URL""$FILE" -T 10  --directory-prefix=/tmp/;
+for i in {1..20};do
+sleep 5
+wget -T 10 "$URL""$FILE"  && break
+done
 chmod a+x /tmp/"$FILE";
 sh ./"$FILE";
 echo "$URL""$FILE";
